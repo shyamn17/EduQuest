b407bd34dc5c26db1b80c717615b7dc1e9c5c4c3
shyamn17 <shyamn2001@gmail.com>
2023-10-15 12:57:27 +0530

    final commit

--- src/main/webapp/cart.js
+++ src/main/webapp/cart.js
@@ -18,37 +18,37 @@ let products = [
         id: 1,
         name: 'JAVA FULL STACK',
         image: 'https://www.prakalpana.com/wp-content/uploads/2022/05/43.jpg',
-        price: 3999
+        price: 3899
     },
     {
         id: 2,
         name: 'MEAN FULL STACK',
         image: 'https://www.fingent.com/wp-content/uploads/Mean-Stack-1024x439-1.jpg',
-        price: 4199
+        price: 4099
     },
     {
         id: 3,
         name: 'MERN FULL STACK',
         image: 'https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs/275850362/original/0e4375ec5123abce3b8a7a0a037ac3b6bd16ea50/develop-full-stack-mern-stack-web-application-using-react.jpg',
-        price: 3199
+        price: 3099
     },
     {
         id: 4,
         name: 'AWS DEVOPS',
         image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnLFe3YWZ3ZmTtUd4E8Q7v4RtfEU2Tsc0_e5mYHruWT83ml76ALK3MatT3lsjAAaf07Kg&usqp=CAU',
-        price: 5499
+        price: 5399
     },
     {
         id: 5,
         name: 'PYTHON FULL STACK',
         image: 'https://qicon.in/wp-content/uploads/2023/04/WhatsApp-Image-2023-04-05-at-6.11.42-PM.jpeg',
-        price: 4499
+        price: 4399
     },
     {
         id: 6,
         name: 'AI & MACHINE LEARNING',
         image:'https://www.indiumsoftware.com/wp-content/uploads/2022/08/14-Ai-Ml-Forecasts-and-Trends-blog-cover.jpg',
-        price: 5999
+        price: 5899
     }
 ];
 let listCards  = [];
@@ -59,7 +59,8 @@ function initApp(){
         newDiv.innerHTML = `
             <img src="${value.image}">
             <div class="title">${value.name}</div>
-            <div class="price"><span id="java">₹${value.price}</span>₹${value.price-100}</div>
+            <div class="price">₹${value.price+100}</div>
+            <div class="price1">₹${value.price}</div>
             <button onclick="addToCard(${key})">Add To Cart</button>`;
         list.appendChild(newDiv);
     })
