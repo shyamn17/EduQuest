f3b5f19680f698ec58daa93bce7ff4622798856b
shyamn17 <shyamn2001@gmail.com>
2024-05-30 17:24:46 +0530

    made homepg,courses responsive

--- src/main/webapp/home.js
+++ src/main/webapp/home.js
@@ -1,3 +1,4 @@
+window.addEventListener('DOMContentLoaded', (event) => {
 var h1 = document.querySelector("h1");
 var btn1=document.querySelector("#but1");
 var btn2=document.querySelector("#but2");
@@ -18,29 +19,24 @@ var data = [
 function updateContent() {
   h1.innerText = data[num].name1;
   btn1.style.borderColor = "";
+  btn2.style.borderColor = "";
+  btn3.style.borderColor = "";
   h1.classList.remove("slide"); 
   setTimeout(function () {
     btn1.style.borderColor = "";
-      h1.innerText = data[num].name1;
-     
-  if(h1.innerText=="O V E R V I E W"){
-  btn1.style.borderColor = "#133e3f";
-
-  }
-  else if(h1.innerText=="C O D I N G </>"){
-  btn2.style.borderColor = "#133e3f";
-
-  }
-  else(h1.innerText=="C O U R S E S")
-  {
-  btn3.style.borderColor = "#133e3f";
-  }
-  num = (num + 1) % data.length;
-  h1.classList.add("slide"); 
-}, 10);
+    h1.innerText = data[num].name1;
+   
+    if (h1.innerText == "O V E R V I E W") {
+      btn1.style.borderColor = "#133e3f";
+    } else if (h1.innerText == "C O D I N G </>") {
+      btn2.style.borderColor = "#133e3f";
+    } else if (h1.innerText == "C O U R S E S") {
+      btn3.style.borderColor = "#133e3f";
+    }
+    num = (num + 1) % data.length;
+    h1.classList.add("slide"); 
+  }, 10);
 }
 setInterval(updateContent, 3000);
-let btn=document.querySelector("#span")
-btn.addEventListener("click", ()=>{
-    window.location.href="./courses.html"
-})
+
+});
