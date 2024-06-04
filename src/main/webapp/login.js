55cfa748d43782685a37a39dd8858b0d9462b9fb
shyamn17 <139563332+shyamn17@users.noreply.github.com>
2024-06-05 02:16:35 +0530

    Update login.js

--- src/main/webapp/login.js
+++ src/main/webapp/login.js
@@ -1,82 +1,90 @@
-let email=document.querySelector(".email");
-let pwd=document.querySelector(".pwd");
-let sub=document.querySelector(".sub");
-let sign=document.querySelector("#reg");
-sign.addEventListener("click",()=>{
-  window.location.href="./signup.html"
-})
-console.log(sub);
+document.addEventListener('DOMContentLoaded', (event) => {
+  let email = document.querySelector("#username");
+  let pwd = document.querySelector("#password");
+  let sub = document.querySelector(".sub");
+  let sign = document.querySelector("#reg");
 
-function checkUser(arr)
-{
-  console.log(arr);
-var NameVal=email.value;
-var passVal=pwd.value;
-console.log(d);
-var d= arr.some((ele)=>{return (ele.password===passVal && ele.name===NameVal)})
-return d
-}
+  sign.addEventListener("click", () => {
+      window.location.href = "./signup.html";
+  });
 
-
-
-function validate(passans,nameans){
-  // var nameans=name1.value
-  // console.log(nameans);
-  // var passans=pass.value
-  // console.log(passans);
-  let lc=0;
-  let up=0;
-  let sc=0;
-  let num=0;
-   let flage=true;
-    for (let index = 0; index < passans.length; index++) {
-      const element = passans[index];
-      if(element!=" ")
-      {if(element>="a"&&element<="z"){ lc++;}
-        else if(element>="A"&&element<="Z"){up++;}
-        else if(element>="0"&&element<="9"){num++}
-        else{sc++}
-
-      }
-    }
-   console.log(nameans);
-  if((nameans[0]>="a" && nameans[0]<="z")||(nameans[0]>="A" && nameans[0]<="Z")){
-    console.log("valid name");
-  }else{console.log("invalid name");
-flage=false}
-  if(passans.length==" "){
-      console.log("invalid password"); flage=false;
-  }else if(lc>=1 && up>=1 && num>=1 && sc>=1){
-    console.log("valid password");
+  function checkUser(arr) {
+      var NameVal = email.value;
+      var passVal = pwd.value;
+      var userExists = arr.some((ele) => {
+          return (ele.password === passVal && ele.name === NameVal);
+      });
+      return userExists;
   }
-  else {console.log("invalid password");; flage=false}
 
-  return flage
-}   sub.addEventListener("click",(event)=>{
-    // th1.innerText="Name"
-    // th2.innerText="Password"
-    event.preventDefault() 
-    // console.log(name1.value)  
-    // console.log(pass.value)
+  function validate(passans, nameans) {
+      let lc = 0;
+      let up = 0;
+      let sc = 0;
+      let num = 0;
+      let isValid = true;
 
-console.log("hi");
-    var passans=pwd.value;
-    var nameans=email.value;
-    console.log(localStorage.getItem("dataList"));
-   if(validate(passans,nameans))
-{
-  console.log( checkUser(JSON.parse(localStorage.getItem("datalist"))))
-   if(checkUser(JSON.parse(localStorage.getItem("datalist")))){
-    alert("Login successful ")
-    window.location.href="./home.html"
-   }else{
-    alert("Invalid username or password")
-   }
+      for (let index = 0; index < passans.length; index++) {
+          const element = passans[index];
+          if (element !== " ") {
+              if (element >= "a" && element <= "z") {
+                  lc++;
+              } else if (element >= "A" && element <= "Z") {
+                  up++;
+              } else if (element >= "0" && element <= "9") {
+                  num++;
+              } else {
+                  sc++;
+              }
+          }
+      }
 
+      if ((nameans[0] >= "a" && nameans[0] <= "z") || (nameans[0] >= "A" && nameans[0] <= "Z")) {
+          console.log("valid name");
+      } else {
+          console.log("invalid name");
+          isValid = false;
+      }
 
-} })
-var cart=document.querySelector("#span")
-cart.addEventListener('click',() => {
-const url='./cart.html'
-window.location.href=url;
-})
+      if (passans.length === 0) {
+          console.log("invalid password");
+          isValid = false;
+      } else if (lc >= 1 && up >= 1 && num >= 1 && sc >= 1) {
+          console.log("valid password");
+      } else {
+          console.log("invalid password");
+          isValid = false;
+      }
+
+      return isValid;
+  }
+
+  sub.addEventListener("click", (event) => {
+      event.preventDefault();
+      
+      var passans = pwd.value;
+      var nameans = email.value;
+      
+      if (validate(passans, nameans)) {
+          let dataList = JSON.parse(localStorage.getItem("datalist"));
+          console.log("DataList from localStorage:", dataList);
+          if (dataList) {
+              console.log("Check User Result:", checkUser(dataList));
+              if (checkUser(dataList)) {
+                  alert("Login successful");
+                  window.location.href = "./home.html";
+              } else {
+                  alert("Invalid username or password. Please try again.");
+              }
+          } else {
+              alert("User data not found. Please sign up.");
+          }
+      }
+  });
+
+  var cart = document.querySelector("#span");
+  cart.addEventListener('click', () => {
+      const url = './cart.html';
+      window.location.href = url;
+  });
+});
